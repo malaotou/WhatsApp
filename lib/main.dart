061294110dba8d90d4809369ed49b53069facb65
lay <lay@lays-Mac.lan>
2019-01-23 06:53:49 -0800

    little change

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,57 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: HomePage(title: 'Flutter Demo Home Page1'),
+      debugShowMaterialGrid: false,
+      showSemanticsDebugger: false,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatefulWidget{
-  HomePage({Key key,this.title}) : super(key:key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
   @override
-  _HomePageState createState()=> _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
- 
-class _HomePageState extends State<HomePage>{
-    @override
-    Widget build (BuildContext context){
-      return Scaffold(
 
-      );
-    }
-    
+class _HomePageState extends State<HomePage> {
+  List<Widget> containers = [
+    Container(color: Colors.green),
+    Container(color: Colors.red),
+    Container(color: Colors.yellow)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color(0xFF1D5D51),
+            title: Text("MyWhatsapp"),
+            centerTitle: false,
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.search), onPressed: () {}),
+              IconButton(icon: Icon(Icons.message), onPressed: () {}),
+              IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+            ],
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: "CALLS",
+                ),
+                Tab(
+                  text: "CHATS",
+                ),
+                Tab(
+                  text: "CONTACTS",
+                )
+              ],
+            )),
+        body: TabBarView(children: containers),
+      ),
+    );
   }
+}
